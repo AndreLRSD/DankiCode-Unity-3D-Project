319c85df44efe5af49873e737830d46bba1ed581
AndreLRSD <119087469+AndreLRSD@users.noreply.github.com>
2022-11-27 19:04:58 -0300

    Game commit

---
+++ My project 3d/Assets/Scripts/CombatEnemy.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CombatEnemy : MonoBehaviour
+{
+    [Header("Atributtes")]
+    public float totalHealth;
+    public float attackDamage;
+    public float movementSpeed;
+    public float lookRadius;
+    public float colliderRadius = 2f;
+    public float rotationSpeed;
+
+    [Header("Components")]
+    private Animator anim;
+    private CapsuleCollider capsule;
+    private NavMeshAgent nav;
+
+    [Header("Other")]
+    private Transform player;
+    private bool walking;
+    private bool attacking;
+    private bool waitFor;
+    private bool hiting;
+    private bool playerAlive;
+
+    [Header("WayPoints")]
+    public List<Transform> wayPoints = new List<Transform>();
+    public int currentPathIndex;
+    public float pathDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        capsule = GetComponent<CapsuleCollider>();
+        nav = GetComponent<NavMeshAgent>();
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (totalHealth > 0)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+
+            if (distance <= lookRadius)
+            {
+                nav.isStopped = false;
+
+                if (!attacking)
+                {
+                    nav.SetDestination(player.position);
+                    anim.SetBool("Walk Forward", true);
+                    walking = true;
+                }
+                if (distance <= nav.stoppingDistance)
+                {
+                    StartCoroutine("Attack");
+                    LookTarget();
+                }
+                else
+                {
+                    attacking = false;
+                }
+            }
+            else
+            {
+                //nav.isStopped = true;
+                anim.SetBool("Walk Forward", false);
+                walking = false;
+                attacking = false;
+                MoveToWayPoint();
+            }
+        }
+    }
+
+    void MoveToWayPoint()
+    {
+        if(wayPoints.Count > 0)
+        {
+            float distance = Vector3.Distance(wayPoints[currentPathIndex].position, transform.position);
+            nav.destination = wayPoints[currentPathIndex].position;
+
+            if(distance <= pathDistance)
+            {
+                currentPathIndex = Random.Range(0, wayPoints.Count);
+            }
+
+            anim.SetBool("Walk Forward", true);
+            walking = true;
+        }
+    }
+
+
+    IEnumerator Attack()
+    {
+        if(!waitFor && !hiting && !playerAlive)
+        {
+            waitFor = true;
+            attacking = true;
+            walking = false;
+            anim.SetBool("Walk Forward", false);
+            anim.SetBool("Bite Attack", true);
+
+            yield return new WaitForSeconds(1.2f);
+
+            GetPlayer();
+
+           // yield return new WaitForSeconds(1f);
+            waitFor = false;
+        }
+
+        if(playerAlive)
+        {
+            anim.SetBool("Walk Forward", false);
+            anim.SetBool("Bite Attack", false);
+            walking = false;
+            attacking = false;
+            nav.isStopped = true;
+        }
+
+
+    }
+
+    void GetPlayer()
+    {
+ 
+        foreach (Collider c in Physics.OverlapSphere(transform.position + transform.forward * colliderRadius, colliderRadius))
+        {
+            if (c.gameObject.CompareTag("Player"))
+            {
+                c.gameObject.GetComponent<Player>().Gethit(attackDamage);
+                playerAlive = c.gameObject.GetComponent<Player>().isDead;
+            }
+        }
+    }
+
+    public void Gethit(float damage)
+    {
+        totalHealth -= damage;
+
+        if (totalHealth > 0)
+        {
+            StopCoroutine("Attack");
+            anim.SetTrigger("Take Damage");
+            hiting = true;
+            StartCoroutine("RecoveryFromHit");
+        }
+        else 
+        {
+            anim.SetTrigger("Die");
+        }
+    }
+
+    IEnumerator RecoveryFromHit()
+    {
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("Walk Forward", false);
+        anim.SetBool("Bite Attack", false);
+        hiting = false;
+        waitFor = false;
+    }
+
+    void LookTarget()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ My project 3d/Assets/Scripts/Player.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private CharacterController controller;
+    public float speed;
+    public float gravity;
+    public float damage;
+    public float totalHealth;
+
+    private Animator anim;
+
+    public float smoothRotTime;
+    private float turnSmoothVelocity;
+
+    public float colliderRadius;
+    public List<Transform> enemyList = new List<Transform>();
+
+    private Transform cam;
+    private bool walking;
+    private bool waitFor;
+    private bool isHit;
+    public bool isDead;
+
+    Vector3 moveDirection;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isDead)
+        { 
+            Move();
+            GetMouseInput();
+        }
+
+    }
+
+    void Move()
+    {
+        if (controller.isGrounded)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+            if (direction.magnitude > 0)
+            {
+                if (!anim.GetBool("Attack"))
+                {
+                    float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                    float smothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
+
+                    transform.rotation = Quaternion.Euler(0f, smothAngle, 0f);
+
+                    moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward * speed;
+
+                    anim.SetInteger("Transition", 1);
+                    walking = true;
+                }
+                else
+                {
+                    anim.SetBool("Walk", false);
+                    
+                    moveDirection = Vector3.zero;
+                }
+
+            }
+            else if(walking && !anim.GetBool("Attack"))
+            {
+                anim.SetBool("Walk", false);
+                moveDirection = Vector3.zero;
+                anim.SetInteger("Transition", 0);
+
+                walking = false;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void GetMouseInput()
+    {
+        if(controller.isGrounded)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(anim.GetBool("Walk"))
+                {
+                    anim.SetBool("Attack", false);
+                    anim.SetInteger("Transition", 0);
+                }
+
+                if(!anim.GetBool("Walk"))
+                {
+                    StartCoroutine("Attack");
+                }
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        if (!waitFor && !isHit)
+        {
+            waitFor = true;
+            anim.SetBool("Attack", true);
+            anim.SetInteger("Transition", 2);
+            yield return new WaitForSeconds(0.4f);
+
+            GetEnemiesList();
+
+            foreach (Transform e in enemyList)
+            {
+                CombatEnemy enemy = e.GetComponent<CombatEnemy>();
+
+                if (enemy != null)
+                {
+                    enemy.Gethit(damage);
+                }
+            }
+
+            yield return new WaitForSeconds(1f);
+            anim.SetInteger("Transition", 0);
+            anim.SetBool("Attack", false);
+            waitFor = false;
+        }
+    }
+
+    void GetEnemiesList()
+    {
+        enemyList.Clear();
+        foreach(Collider c in Physics.OverlapSphere(transform.position + transform.forward * colliderRadius, colliderRadius))
+        {
+            if(c.gameObject.CompareTag("Enemy"))
+            {
+                enemyList.Add(c.transform);
+            }
+        }
+    }
+
+    public void Gethit(float damage)
+    {
+        totalHealth -= damage;
+
+        if (totalHealth > 0)
+        {
+            StopCoroutine("Attack");
+            anim.SetInteger("Transition", 3);
+            isHit = true;
+            StartCoroutine("RecoveryFromHit");
+        }
+        else
+        {
+            isDead = true;
+            anim.SetTrigger("Die");
+        }
+    }
+
+    IEnumerator RecoveryFromHit()
+    {
+        yield return new WaitForSeconds(1f);
+        anim.SetInteger("Transition", 0);
+        isHit = false;
+        anim.SetBool("Attack", false);
+      
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + transform.forward, colliderRadius);
+    }
+}
